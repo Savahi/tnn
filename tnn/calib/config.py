9bda1b5f01feefdad515d25875786705ed4cca69
timujin <newtimujin@gmail.com>
2017-10-14 14:49:16 +0300

    Calib

--- tnn/calib/config.py
+++ tnn/calib/config.py
@@ -8,8 +8,8 @@ params = {
 	
 	# Либо создать новую
 	"network": {
-		"nodes": [24,16,10],
-		"num_inputs": 30,
+		"nodes": [36,16,10],
+		"num_inputs": 10,
 		"bins": 3,
 		"activationFuncs": ["sigmoid","sigmoid","sigmoid"],
 	},
@@ -18,17 +18,17 @@ params = {
 	"raw_file":"RTS_1h_150820_170820.txt",
 
 	# Функции формирования данных; None=поведение по умолчанию
-	"calcData": calc_data,
+	"calcData": None, #calc_data,
 
 	# Параметры обучения
-	"learningRate":0.05,
-	"numEpochs":10000,
+	"learningRate":0.050,
+	"numEpochs":2000,
 	"optimizer":'GradientDescent',
 
 	# папка, куда tensorflow пишет summary ("отчет"). 
         # Если summaryDir==None, отчеты записываться не будут.
         # Если summaryDir=="", то имя папки будет сгенерировано автоматически из текущих даты и времени (только числа, без других знаков).
-        "summaryDir":'',
+        "summaryDir":'TIMTEST',
 	
 	# если задать True, в процессе обучения, для каждой эпохи будут записываться
         # пары значений (для train и test данных): 
@@ -43,5 +43,5 @@ params = {
 
 	#имя папки, в которую будет сохраннен файл с весами сети.
         #Если saveDir==None, имя папки будет сгенерировано на основе текущих даты и времени.
-	"saveDir":None
+	"saveDir":"TIMTEST"
 }
